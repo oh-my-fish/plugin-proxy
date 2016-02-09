00a2955abf4222a35731ffef053463adb0994b1d
James Hillyerd <james@hillyerd.com>
2016-02-08 16:49:25 -0800

    Implement uninstall function

--- uninstall.fish
+++ uninstall.fish
@@ -1,3 +1,14 @@
 function uninstall --on-event uninstall_proxy
+  # Unset proxy variables
+  set -l envars http_proxy HTTP_PROXY \
+  https_proxy HTTPS_PROXY \
+  ftp_proxy FTP_PROXY \
+  all_proxy ALL_PROXY
+  for envar in $envars
+    set -e $envar
+  end
+
+  # Erase proxy functions
+  functions -e noproxy proxy __proxy.set
 end
 
