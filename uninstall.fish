5e4c166f951f4aa41f95acb988e3e79341976453
Justin Hileman <justin@justinhileman.info>
2016-03-06 13:28:30 -0800

    Uninstall immediately, rather than waiting for an event.

--- uninstall.fish
+++ uninstall.fish
@@ -1,14 +1,12 @@
-function uninstall --on-event uninstall_proxy
-  # Unset proxy variables
-  set -l envars http_proxy HTTP_PROXY \
+# Unset proxy variables
+set -l envars http_proxy HTTP_PROXY \
   https_proxy HTTPS_PROXY \
   ftp_proxy FTP_PROXY \
   all_proxy ALL_PROXY
-  for envar in $envars
-    set -e $envar
-  end
 
-  # Erase proxy functions
-  functions -e noproxy proxy __proxy.set
+for envar in $envars
+  set -e $envar
 end
 
+# Erase proxy functions
+functions -e noproxy proxy __proxy.set
