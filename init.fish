d675688a350682c84763bf3521cfd40ab6f20935
Justin Hileman <justin@justinhileman.info>
2016-03-06 13:06:09 -0800

    Initialize immediately, rather than waiting for an event.

--- init.fish
+++ init.fish
@@ -1,6 +1,3 @@
-function init -a path --on-event init_proxy
-    if set -q proxy_host
-       __proxy.set "http://$proxy_host"
-    end
+if set -q proxy_host
+  __proxy.set "http://$proxy_host"
 end
-
